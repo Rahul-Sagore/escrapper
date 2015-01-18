f3d23d7f8f8ca404d95a77d855f336e988726f22
Rahul-Sagore <rahul.sagore@gmail.com>
2015-01-18 12:06:47 +0530

    Added code for form data processing

--- escrapper.py
+++ escrapper.py
@@ -1,14 +1,27 @@
+# Importing flask, url_for - URL generation, render_template - Jinja template
 from flask import Flask, url_for
-from flask import render_template
-app = Flask(__name__)
+from flask import render_template, flash, redirect
+from forms import SearchForm
+import scrapping
 
-@app.route('/')
-def hello_world():
-	return render_template('index.html')
+app = Flask(__name__)
+app.config.from_object('config')
+
+@app.route('/index/')
+@app.route('/', methods=['GET', 'POST'])
+def home():
+	form = SearchForm()
+	result = {}
+	#validating and processing form data
+	if form.validate_on_submit():
+		flash('Product  : %s' % (form.product_name.data))
+		result = scrapping.scrap_input(form.product_name.data)
+		return redirect('/')
+	
+	return render_template('index.html', form=form, result=result)
 
 with app.test_request_context():
 	url_for('static', filename='skeleton.css')
 
 if __name__ == '__main__':
-	
     app.run(debug=True)
