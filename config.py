cc58d3743394c9e7178872c54cb4427dc6d1b514
Rahul-Sagore <rahul.sagore@gmail.com>
2015-01-18 12:11:15 +0530

    Added form configuration file for flask extensions

---
+++ config.py
@@ -0,0 +1,4 @@
+#Configuration for flask-wtf form extension
+#The WTF_CSRF_ENABLED setting activates the cross-site request forgery prevention
+WTF_CSRF_ENABLED = True
+SECRET_KEY = 'rascalescrapper'
